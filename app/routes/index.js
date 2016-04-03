cfff9e70ad7f71307ed82cba67963606122100b2
chichavl <chichavl@gmail.com>
2016-04-03 00:00:33 +0000

    final solution

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,46 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
-module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
+module.exports = function (app) {
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+	
+	app.route('/:time')
+		.get(function(req, res) {
+		    console.log(req.params.time);
+		    var monthNames = ["January", "February", "March", "April", "May", "June",
+		    "July", "August", "September", "October", "November", "December"];
+		    var obj = {};
+		    //date in "unix" format
+		    if (Number.isInteger(parseInt(req.params.time))) {
+		    	console.log("unix format");
+		    	obj.unux = req.params.time;
+		    	
+		    	var date = new Date(req.params.time*1000);
+				// Month of date
+				var month = monthNames[date.getMonth()];
+				// Day part from the timestamp
+				var day = "0" + date.getUTCDate()
+				// Year part from the timestamp
+				var year = "0" + date.getFullYear();
+		    	obj.natural = month+ ' ' + day.substr(-2) + ',' + year.substr(-4);
+		    }
+		    else if (Number.isInteger(Date.parse(decodeURI(req.params.time)))) {
+		    	console.log("human format");
+		    	obj.natural = decodeURI(req.params.time);
+		    	
+		    	obj.unix = Date.parse(decodeURI(req.params.time))/1000;
+		    }
+		    else {
+		    	console.log("not a date");
+		    	obj.natural = null;
+		    	obj.unix = null;
+		    }
+		    res.end(JSON.stringify(obj));
 		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
